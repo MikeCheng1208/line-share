16d8bf685a3c53284de1c8c76477cde6e4d5adfd
mike <mike@medialand.com>
2016-09-12 15:56:21 +0800

    line share

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 window.onload = function(){
-
+    //用device.js判斷桌機還是手機來藏按鈕
 	if(device.desktop()){
-        document.getElementById("LinePc").style.display="block";
         document.getElementById("Linemobile").style.display="none";
 	}else{
         document.getElementById("LinePc").style.display="none";
-        document.getElementById("Linemobile").style.display="block";
     }
 	
     document.getElementById("LinePc").onclick = function(){
@@ -14,8 +12,4 @@ window.onload = function(){
     document.getElementById("Linemobile").onclick = function(){
         window.open('http://line.me/R/msg/text/'+ encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
-
-
-
-
 }
