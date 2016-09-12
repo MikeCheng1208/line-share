86d2cea83b36c891273ff4aec999ce12d7818954
mike <mike@medialand.com>
2016-09-12 15:48:07 +0800

    line share

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 window.onload = function(){
+
+	if(device.desktop()){
+        document.getElementById("LinePc").style.display="block";
+        document.getElementById("Linemobile").style.display="none";
+	}else{
+        document.getElementById("LinePc").style.display="none";
+        document.getElementById("Linemobile").style.display="block";
+    }
+	
     document.getElementById("LinePc").onclick = function(){
         window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
     document.getElementById("Linemobile").onclick = function(){
         window.open('http://line.me/R/msg/text/'+ encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
+
+
+
+
 }
