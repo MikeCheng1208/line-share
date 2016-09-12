f93a078332a711045f8b934d813cb39e28aadc61
mike <mike@medialand.com>
2016-09-12 15:05:45 +0800

    LINE share

---
+++ src/main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    
+    document.getElementById("Linemobile").onclick = function(){
+        window.open('http://line.me/R/msg/text/http%3A%2F%2Fwww.hennessy-tw.com%2F2016artofcognac%2F',"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+    }
+
+    document.getElementById("LinePc").onclick = function(){
+        window.open('http://line.me/R/msg/text/http%3A%2F%2Fwww.hennessy-tw.com%2F2016artofcognac%2F',"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+    }
+
+}
---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/greensock/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/require/index.d.ts" />
+/// <reference path="globals/vue/index.d.ts" />
