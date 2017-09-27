e2ff6a85179f5454633068da33ab8601e89c457f
MikeCheng1208 <a3804430@hotmail.com>
2017-09-27 16:59:35 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ window.onload = function(){
     }
 	
     document.getElementById("LinePc").onclick = function(){
-        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('https://lineit.line.me/share/ui?url='+encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
     document.getElementById("Linemobile").onclick = function(){
-        window.open('line://msg/text/'+ encodeURIComponent("https://a3804430.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
+        window.open('line://msg/text/'+ encodeURIComponent("https://mikecheng1208.github.io/LineShare_Example/src/"),"_blank","toolbar=yes,location=yes,directories=no,status=no, menubar=yes,scrollbars=yes,resizable=no, copyhistory=yes,width=600,height=400")
     }
 }
